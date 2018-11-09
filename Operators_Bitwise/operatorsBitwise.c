6c90deea5168d4235a8ef5e58b3eeb64fbf70e85
Matthew Shelbourn <mattshelbourn@gmail.com>
2018-11-08 23:15:20 -0800

    Uploading bitwise operators examples.

---
+++ Operators_Bitwise/operatorsBitwise.c
@@ -0,0 +1,62 @@
+/*  Author: Matthew Shelbourn
+    Copyright 2018 Matthew Shelbourn
+    Date: 11/08/2018
+    Subject: Udemy - Bitwise Operators */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    // and (&) bitwise operator
+
+    unsigned int a = 60; // 0011 1100 (60 in binary code)
+    unsigned int b = 13; // 0000 1101 (13 in binary code)
+    int result = 0;
+
+    result = a & b; // a & b in binary is: 0000 1100 (values are only true if both a and b are true
+
+    printf("The result of a & b is: %d\n", result);
+
+    // or (|) bitwise operator
+
+    unsigned int c = 45; // 0010 1101 (45 in binary code)
+    unsigned int d = 76; // 0100 1100 (76 in binary code)
+    int result2 = 0;
+
+    result2 = c | d; // c | d in binary is: 0110 1101 (109)
+
+    printf("The result of c | d is: %d\n", result2);
+
+    // exclusive or (^) bitwise operator
+
+    unsigned int e = 108; // 0110 1100 (108 in binary code)
+    unsigned int f = 55; // 0011 0111 (55 in binary code)
+    int result3 = 0;
+
+    result3 = e ^ f; // e ^ f in binary is: 0101 1011 (91)
+
+    printf("The result of e ^ f is: %d\n", result3);
+
+    // complement (~) bitwise operator
+
+    unsigned char g = 154; // 1001 1010 (154 in binary code)
+    unsigned char h = 100; // 0110 0100 (100 in binary code)
+    int result4 = 0;
+
+    result4 = ~g; // (~) bitwise operator is unary and flips the binary values
+
+    printf(" The result of ~g is: %d\n", result4);
+
+    // left (<<) and right (>>) shift  bitwise operators
+
+    unsigned int i = 80; // 0101 0000 (80 in binary code)
+    int result5 = 0;
+
+    result5 = i << 2; // shifts bits of i to left by two spaces
+                     // 0100 0010 is the new value after the shift
+
+    printf("The result of i << 2 is: %d\n", result5);
+
+    return 0;
+}
