bf131e51972aae1c2ada81de577a82784580ece8
Matthew Shelbourn <mattshelbourn@gmail.com>
2018-11-14 22:55:37 -0800

    Updating comments in conditionalOPerator program.

--- conditionalOperator/conditionalOperator.c
+++ conditionalOperator/conditionalOperator.c
@@ -14,7 +14,9 @@ int main(void)
     /*  The below if-else statement uses the conditional operator, which is a ternary operator
         because it has 3 operands. In this example, the statement is shorthand for "If y is less
         than 3 then assign 32 to x. Else, assign 8 to x. X is not part of the actual conditional
-        statement, it's value is being assigned based on the output of the if-else statement. */
+        statement, it's value is being assigned based on the output of the if-else statement.
+        A max of two expressions can be used with the conditional operator since brackets
+        cannot be used. */
 
     x = y < 3 ? 32 : 8; // using the conditional operator ? :
 
