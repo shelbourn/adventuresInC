8bc25252637217665284dc5f29cf008d8bd8ce9d
Matthew Shelbourn <mattshelbourn@gmail.com>
2018-11-14 21:45:19 -0800

    Updating challenge answer to be more informational.

--- Challenge5_SizeOf_Operator/challenge4SizeOfOperator.c
+++ Challenge5_SizeOf_Operator/challenge4SizeOfOperator.c
@@ -8,12 +8,15 @@
 
 int main()
 {
-    printf("The size of an int data type is: %d\n", sizeof(int));
-    printf("The size of a char data type is: %d\n", sizeof(char));
-    printf("The size of a long data type is: %d\n", sizeof(long));
-    printf("The size of a long long data type is: %d\n", sizeof(long long));
-    printf("The size of a double data type is: %d\n", sizeof(double));
-    printf("The size of a long double data type is: %d\n", sizeof(long double));
+    printf("The size of an int data type is: %u bytes\n", sizeof(int));
+    printf("The size of a char data type is: %u bytes\n", sizeof(char));
+    printf("The size of a short data type is: %u bytes\n", sizeof(short));
+    printf("The size of a float data type is: %u bytes\n", sizeof(float));
+    printf("The size of a long data type is: %u bytes\n", sizeof(long));
+    printf("The size of a long long data type is: %u bytes\n", sizeof(long long));
+    printf("The size of a double data type is: %u bytes\n", sizeof(double));
+    printf("The size of a long double data type is: %u bytes\n", sizeof(long double));
+
 
     return 0;
 }
